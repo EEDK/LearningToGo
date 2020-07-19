2f1d70bea93dd3d5103277d9c6c279e11589505d
kim dong eon <kde9604@naver.com>
2020-07-19 19:42:51 +0900

    answeringQuestion8

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 )
 
-func add(x int , y int) int {
-	return x + y
+func stringSwap(x , y string) (string, string){
+	return y, x
 }
 
 func main() {
-	fmt.Println(add(42 , 13))
+	a, b := stringSwap("hello", "world")
+	fmt.Println(a, b)
 }
 
