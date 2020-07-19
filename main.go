7622fa56f6813f706b64dcfa79a1950898e54451
kim dong eon <kde9604@naver.com>
2020-07-19 21:30:12 +0900

    values

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"math"
+import "fmt"
+
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
 )
 
-const Pi = math.Pi
-
-func main() {
-	const World = "안녕"
-	fmt.Println("Hello" , World)
-	fmt.Println("Haapy" , Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go rules" , Truth)
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
 }
 
+func main() {
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+}
