39c4a470832d5fb253ba6c3ddf7f406d847a2e94
kim dong eon <kde9604@naver.com>
2020-07-19 19:45:21 +0900

    Add named result

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func stringSwap(x , y string) (string, string){
-	return y, x
+func split(sum int)(x, y int){
+	x = sum * 4/9
+	y = sum - x
+	return
 }
 
 func main() {
-	a, b := stringSwap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
 
