3065b4467e3b19877aa198da105758c7e9a1cd84
kim dong eon <kde9604@naver.com>
2020-07-20 21:39:29 +0900

    Slice

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X , Y int
-}
-
-
 func main() {
-	v := new(Vertex)
-	fmt.Println(v)
-	v.X, v.Y = 11, 9
-	fmt.Print(v)
+	p := []int{2,3,5,7,11,13}
+	fmt.Println("p == ", p)
+
+	for i := 0; i < len(p); i++{
+		fmt.Printf("p[%d] == %d\n", i , p[i])
+	}
 }
