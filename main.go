ab22e1bc5f621af440048aae6a542574efdbb205
kim dong eon <kde9604@naver.com>
2020-07-20 21:29:47 +0900

    Add struct

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"math/cmplx"
-)
+import "fmt"
 
-var (
-	ToBe	bool		= false
-	MaxInt 	uint64		= 1<<64 - 1
-	z		complex128	= cmplx.Sqrt(-5 + 12i)
-)
+type Vertex struct {
+	X int
+	Y int
+}
 
 func main() {
-	const f = "%T(%v)\n"
-	fmt.Printf(f , ToBe, ToBe)
-	fmt.Printf(f, MaxInt, MaxInt)
-	fmt.Printf(f, z, z)
+	fmt.Println(Vertex{1, 2})
 }
